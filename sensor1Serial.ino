fa6f8d66731b682d82ea9368d1ba717ddc1be9c5
SrayaGoldfisher <47556043+SrayaGoldfisher@users.noreply.github.com>
2021-06-28 15:20:46 +0300

    Add a description

--- sensor1Serial.ino
+++ sensor1Serial.ino
@@ -1,90 +1,81 @@
-
- int Anin0 = 0;                 
- int Anin1 = 1;                 
- int Anin2 = 2;                 
- int Anin3 = 3;                 
- int Anin4 = 4;                 
- int Anin5 = 5;                 
-            
-
-int v[6]={};
-
- void setup() {
-     
-  Serial.begin(9600);
- }
+//This code sends digital signals from Arduino to MATLAB that express analog voltage coming from a network of 7 resistors
 
 
- void loop() {
-     
-  //setup for sample 1: 3 vcc 4 gound 
-  pinMode(A0, INPUT); 
-  pinMode(A1, INPUT);
-  pinMode(A4, INPUT);
-  pinMode(A5, INPUT);
-  pinMode(A2, OUTPUT);
-  pinMode(A3, OUTPUT);
-  digitalWrite(A2, HIGH); 
-  digitalWrite(A3, LOW);     
-  
-  delay(10);
-  v[0] = analogRead(Anin0);
-  delay(10);  
-  v[1] = analogRead(Anin1);   
-  delay(10);  
-  v[2] = 1023;             
-  v[3] = 0;
-  v[4] = analogRead(Anin4);   
-  delay(10);             
-  v[5] = analogRead(Anin5); 
-  delay(10);
+int Anin0 = 0;      
+int Anin1 = 1;                 
+int Anin2 = 2;                 
+int Anin3 = 3;                 
+int Anin4 = 4;                 
+int Anin5 = 5;                 
+           
 
+int v[6] = {};
 
-
-  for(int i=0;i<6;i++){
-  while (Serial.available() < 1) 
-  {} 
-  Serial.read();  
-  Serial.println(v[i]);
-      
-     
-  }
-
-
-  //setup for sample 2: 1 - vcc 6 - ground 
-  
-  pinMode(A1, INPUT); 
-  pinMode(A2, INPUT);
-  pinMode(A3, INPUT);
-  pinMode(A4, INPUT);
-  pinMode(A0, OUTPUT);
-  pinMode(A5, OUTPUT);
-  digitalWrite(A0, HIGH);
-  digitalWrite(A5, LOW);
-
-  v[0] = 1023;   
-  delay(10);             
-  v[1] = analogRead(Anin1); 
-  delay(10);
-  v[2] = analogRead(Anin2); 
-  delay(10);
-  v[3] = analogRead(Anin3); 
-  delay(10);
-  v[4] = analogRead(Anin4);   
-  delay(10);             
-  v[5] = 0; 
-
-
-  for(int i=0;i<6;i++){
-  while (Serial.available() < 1) 
-  {} 
-  Serial.read();  
-  Serial.println(v[i]);
-      
-     
-  }
-
+void setup() {
+    Serial.begin(9600);
 }
 
- 
 
+void loop() {
+     
+    //setup for sample 1: 3 vcc 4 gound 
+    pinMode(A0, INPUT); 
+    pinMode(A1, INPUT);
+    pinMode(A4, INPUT);
+    pinMode(A5, INPUT);
+    pinMode(A2, OUTPUT);
+    pinMode(A3, OUTPUT);
+    digitalWrite(A2, HIGH); 
+    digitalWrite(A3, LOW);     
+  
+    delay(10);
+    v[0] = analogRead(Anin0);
+    delay(10);  
+    v[1] = analogRead(Anin1);   
+    delay(10);  
+    v[2] = 1023;             
+    v[3] = 0;
+    v[4] = analogRead(Anin4);   
+    delay(10);             
+    v[5] = analogRead(Anin5); 
+    delay(10);
+
+
+    for(int i=0;i<6;i++){
+        while(Serial.available() < 1) {
+        } 
+        Serial.read();  
+        Serial.println(v[i]); 
+    }
+
+
+    //setup for sample 2: 1 - vcc 6 - ground 
+    pinMode(A1, INPUT); 
+    pinMode(A2, INPUT);
+    pinMode(A3, INPUT);
+    pinMode(A4, INPUT);
+    pinMode(A0, OUTPUT);
+    pinMode(A5, OUTPUT);
+    digitalWrite(A0, HIGH);
+    digitalWrite(A5, LOW);
+
+    v[0] = 1023;   
+    delay(10);             
+    v[1] = analogRead(Anin1); 
+    delay(10);
+    v[2] = analogRead(Anin2); 
+    delay(10);
+    v[3] = analogRead(Anin3); 
+    delay(10);
+    v[4] = analogRead(Anin4);   
+    delay(10);             
+    v[5] = 0; 
+
+
+    for(int i = 0; i < 6; i++) {
+        while(Serial.available() < 1) {
+        } 
+        Serial.read();  
+        Serial.println(v[i]);
+    }
+}
