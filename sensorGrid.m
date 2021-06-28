a2a7a678630155014b0dfdcea1c3b34c91c8b41f
SrayaGoldfisher <47556043+SrayaGoldfisher@users.noreply.github.com>
2021-06-28 15:06:06 +0300

    Add a description

--- sensorGrid.m
+++ sensorGrid.m
@@ -1,30 +1,33 @@
+%This code receives digital signals from Arduino that express analog voltage coming from a network of 7 resistors.
+%The code calculates a resistor ratio in a circuit by Kirchhoff's laws.
+
+
 function r = sensorGrid()
 %%this code is only for 6 resistense with grid 2*3
 %create by Yaron Samuel and Sraya Goldfisher
-%
 
 
-v=serial1()
+v = serial1()
 
-n=2;
-m=3;
+n = 2;
+m = 3;
 
-size = n*(m-1)+m*(n-1);
+size = n * (m - 1) + m * (n - 1);
 A = zeros(size);
-A(1,:)=[v(1,1)-v(1,2)   0   0   v(1,1)-v(1,3)   0   0   0];
-A(2,:)=[v(1,2)-v(1,1)   0   0   0   v(1,2)-v(1,4)   0   0];
-A(3,:)=[0	0   v(1,5)-v(1,6)   0   0   v(1,5)-v(1,3)   0];
-A(4,:)=[0   0   v(1,6)-v(1,5)   0   0   0   v(1,6)-v(1,4)];
-A(5,:)=[0   v(2,3)-v(2,4)   0   v(2,3)-v(2,1)   0   v(2,3)-v(2,5)   0];
-A(6,:)=[0   v(2,4)-v(2,3)   0   0   v(2,4)-v(2,2)   0   v(2,4)-v(2,6)];
-A(7,:)=[0	0   v(2,5)-v(2,6)   0   0   v(2,5)-v(2,3)   0];
+A(1,:) = [v(1,1)-v(1,2)   0   0   v(1,1)-v(1,3)   0   0   0];
+A(2,:) = [v(1,2)-v(1,1)   0   0   0   v(1,2)-v(1,4)   0   0];
+A(3,:) = [0	0   v(1,5)-v(1,6)   0   0   v(1,5)-v(1,3)   0];
+A(4,:) = [0   0   v(1,6)-v(1,5)   0   0   0   v(1,6)-v(1,4)];
+A(5,:) = [0   v(2,3)-v(2,4)   0   v(2,3)-v(2,1)   0   v(2,3)-v(2,5)   0];
+A(6,:) = [0   v(2,4)-v(2,3)   0   0   v(2,4)-v(2,2)   0   v(2,4)-v(2,6)];
+A(7,:) = [0	0   v(2,5)-v(2,6)   0   0   v(2,5)-v(2,3)   0];
 %dispeqn(A)
 
 disp(A)
 [V,D] = eig(A)
 
 rt = V(: ,7);
-r=1./rt;
+r = 1 ./ rt;
 
  
  
