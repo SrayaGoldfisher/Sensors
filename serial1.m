73c44a9fe79b1def78cdca6ad55006376612764d
SrayaGoldfisher <47556043+SrayaGoldfisher@users.noreply.github.com>
2021-06-28 15:26:58 +0300

    Add a description

--- serial1.m
+++ serial1.m
@@ -1,13 +1,17 @@
-function v = serial1()
+%A communication code that receives signals from Arduino that express an analog voltage 
+%that comes from a network of 7 resistors and puts them into processing in a MATLAB
+
 %%create by Yaron Samuel and Sraya Goldfisher
 
-com='com6';
+function v = serial1()
+
+com = 'com6';
  
 try
-    s=connect(com);
+    s = connect(com);
 catch
     comfix  
-    s=connect(com);
+    s = connect(com);
 end
 
 
@@ -18,17 +22,17 @@ v=zeros(2,6);
 
 %for trush
 fprintf(s,'1');
-v1=str2num(fscanf(s));
+v1 = str2num(fscanf(s));
 
 for k = 1:2
-    for i=1:6
+    for i = 1:6
         fprintf(s,'1');
-        v(k,i)=str2num(fscanf(s));
+        v(k,i) = str2num(fscanf(s));
     end
 end
 
 disp('done!');
 
-v=v*5/1023;
+v = v * 5 / 1023;
 
 end
