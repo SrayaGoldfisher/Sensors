a5bcb77882c5bb9ff91a5e55157ed647519aa436
SrayaGoldfisher <47556043+SrayaGoldfisher@users.noreply.github.com>
2021-12-28 09:20:55 +0200

    Add files via upload

---
+++ Vayyar.cpp
@@ -0,0 +1,44 @@
+GT(x0, y0)
+input(x1, y1
+    x2, y2
+    .....
+    xm, ym);
+output()
+
+
+for (m = 1; m < n; m++) {
+    point(x, y) = point(mx, my);
+    if ((point() - point0) ^ 2 <= (point2 - point0) ^ 2)
+    {
+        point1 = RealPoint;
+    }
+    else
+    {
+        point2 = RealPoint;
+    }
+}
+
+point1 = dist(x1, y1)
+point2 = dist(x2, y2)
+pointm = dist(xm, ym)
+
+if (m > 1)
+{
+    for (point = 1; point < m; point++)
+    {
+        if (point < BestMuch)
+        {
+            BestMuch = point;
+        }
+    }
+}
+else
+{
+    BestMuch = point1;
+}
+
+
+float dist(float)
+{
+
+}
